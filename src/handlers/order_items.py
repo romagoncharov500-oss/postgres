cd36573cbb7056a72eb1d2989d53311e89c3142d
Roman <romagoncharov500@gmail.com>
2026-06-15 21:04:49 +0300

    add item selecting logic

--- src/handlers/order_items.py
+++ src/handlers/order_items.py
@@ -4,14 +4,13 @@ from decimal import Decimal
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit import prompt
 from prompt_toolkit.shortcuts import choice
-from psycopg.rows import class_row
+from psycopg.rows import class_row, scalar_row
 
 from console import console, render_error
 from db import get_conn
 from validators import PositiveIntValidator, YesNoValidator
 from commands import command, CATEGORY_ITEMS
 from handlers.products import Product
-from handlers.orders import _get_order
 
 
 @dataclass
@@ -23,10 +22,22 @@ class Item:
     price: Decimal
 
 
-def _get_all_products() -> list[Product]:
+def _get_availible_products(_order_id: int) -> list[Product]:
     conn = get_conn()
     with conn.cursor(row_factory=class_row(Product)) as cur:
-        cur.execute("SELECT * FROM catalog.products")
+        # Один запрос: выбираем продукты, которых нет в подзапросе для данного заказа
+        cur.execute(
+            """
+            SELECT * 
+            FROM catalog.products 
+            WHERE id NOT IN (
+                SELECT product_id 
+                FROM sales.order_items 
+                WHERE order_id = %s
+            )
+            """,
+            (_order_id,)
+        )
         return cur.fetchall()
     
     
@@ -51,6 +62,7 @@ def _update_order_total(order_id: int) -> None:
 
     conn.execute("UPDATE sales.orders SET total_amount = %s WHERE id = %s", (total_amount_updated, order_id,))
 
+
 def _choose_order_item(order_id: int) -> Item:
     items = _get_order_items(order_id)
     if not items:
@@ -70,26 +82,25 @@ def _choose_order_item(order_id: int) -> Item:
     
     return item_id_to_edit
 
+
 @command("add item", "добавить товары к заказу (интерактивно)", CATEGORY_ITEMS)
 def add_item(_id: str) -> None:
     order_id = int(_id)
-    order = _get_order(order_id)
-    if order is None:
-        return
-
-    products = _get_all_products()
-    if not products:
-        render_error("Каталог товаров пуст. Невозможно добавить позицию.")
-        return
-    
-    completion_mapping = {}
-    for p in products:
-        display_name = f"{p.name} (ID: {p.id}, sku: {p.sku}, Цена: {p.price})"
-        completion_mapping[display_name] = p
-
-    completer = WordCompleter(list(completion_mapping.keys()), ignore_case=True)
 
     while True:
+        products = _get_availible_products(order_id)
+
+        if not products:
+            render_error("Каталог товаров пуст. Невозможно добавить позицию.")
+            return
+
+        completion_mapping = {}
+        for p in products:
+            display_name = f"{p.name} (ID: {p.id}, sku: {p.sku}, Цена: {p.price})"
+            completion_mapping[display_name] = p
+
+        completer = WordCompleter(list(completion_mapping.keys()), ignore_case=True)
+
         console.print("\n[bold cyan]--- Добавление товара в заказ ---[/bold cyan]")
         
         # Выбор товара через автокомплит
@@ -124,9 +135,6 @@ def add_item(_id: str) -> None:
 @command("edit item", "изменить количество товара в заказе", CATEGORY_ITEMS)
 def edit_item(_id: str) -> None:
     order_id = int(_id)
-    order = _get_order(order_id)
-    if order is None:
-        return
 
     item_to_edit = _choose_order_item(order_id)
     if item_to_edit is None:
@@ -148,9 +156,6 @@ def edit_item(_id: str) -> None:
 @command("delete item", "удалить товар из заказа", CATEGORY_ITEMS)
 def delete_item(_id: str) -> None:
     order_id = int(_id)
-    order = _get_order(order_id)
-    if order is None:
-        return
  
     item_id_to_delete = _choose_order_item(order_id)
     if item_id_to_delete is None:
