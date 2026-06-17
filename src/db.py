121cf39f4d8a767cee4f00758daefa9c4b499209
Roman <romagoncharov500@gmail.com>
2026-06-17 21:08:12 +0300

    start auth support

--- src/db.py
+++ src/db.py
@@ -3,6 +3,15 @@ from typing import Final
 import psycopg
 from psycopg import Connection
 
+import os
+
+# Другие импорты тут
+DB_NAME: Final[str] = os.environ["DB_NAME"]
+DB_USER: Final[str] = os.environ["DB_USER"]
+DB_PASSWORD: Final[str] = os.environ["DB_PASSWORD"]
+DB_HOST: Final[str] = os.environ["DB_HOST"]
+DB_PORT: Final[int] = int(os.environ["DB_PORT"])
+
 DB_NAME: Final[str] = "inventorydb"
 DB_USER: Final[str] = "app_user"
 DB_PASSWORD: Final[str] = "pass"
