d190eec42accb4043596cf150677bcc549ee27f5
romagoncharov500-oss <romagoncharov500@gmail.com>
2026-06-21 01:08:49 +0300

    add roles in decorator

--- src/commands.py
+++ src/commands.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 from typing import Final, Sequence, Callable
 
 from prompt_toolkit.completion import NestedCompleter
+from auth import auth_user
 
 # Категории команд
 CATEGORY_GENERAL: Final[str] = "ПРОЧЕЕ"
@@ -27,6 +28,7 @@ class Command:
     handler: Callable[..., None]
     description: str
     category: str
+    allowed_roles: Sequence[str]
     args: Sequence[str] = field(default_factory=tuple)
 
 
@@ -34,7 +36,7 @@ class Command:
 _COMMANDS_REGISTRY: list[Command] = []
 
 
-def command(text: str, description: str, category: str):
+def command(text: str, description: str, category: str, allowed_roles: Sequence[str]):
     """
     Декоратор для регистрации команд.
     Автоматически извлекает аргументы из сигнатуры функции.
@@ -49,6 +51,7 @@ def command(text: str, description: str, category: str):
             handler=func,
             description=description,
             category=category,
+            allowed_roles=allowed_roles,
             args=args,
         )
         _COMMANDS_REGISTRY.append(cmd)
@@ -59,7 +62,9 @@ def command(text: str, description: str, category: str):
 
 def get_commands() -> Sequence[Command]:
     """Возвращает список всех зарегистрированных команд."""
-    return _COMMANDS_REGISTRY
+    user = auth_user()
+    cmds = [c for c in _COMMANDS_REGISTRY if user.role in c.allowed_roles]
+    return cmds
 
 
 def _build_completer_dict() -> dict:
