f95b5a69246b400624448232c10b8fbdb720a82e
romagoncharov500-oss <romagoncharov500@gmail.com>
2026-07-09 01:24:07 +0300

    add list functions

--- src/handlers/orders.py
+++ src/handlers/orders.py
@@ -140,6 +140,38 @@ def _list_orders_by_status(status: str) -> None:
     console.print(table)
 
 
+@command("list orders new", "просмотр новых заказов", CATEGORY_ORDERS, [ROLE_INVENTORY_MANAGER])
+def list_orders_new() -> None:
+    _list_orders_by_status("new")
+
+
+@command("list orders processing", "просмотр заказов в обработке", CATEGORY_ORDERS, [ROLE_INVENTORY_MANAGER])
+def list_orders_processing() -> None:
+    _list_orders_by_status("processing")
+
+
+@command("list orders my", "мои заказы", CATEGORY_ORDERS, [ROLE_INVENTORY_MANAGER])
+def list_orders_my() -> None:
+    conn = get_conn()
+    table = _create_orders_table()
+
+    with conn.cursor(row_factory=class_row(Order)) as cur:
+        cur.execute("SELECT * FROM sales.orders WHERE processed_by = %s", (auth_user().id,))
+        orders: list[Order] = cur.fetchall()
+
+    for order in orders:
+        table.add_row(
+            str(order.id),
+            order.status,
+            str(order.total_amount),
+            str(order.created_at),
+            str(order.warehouse_id),
+            get_user(order.created_by).username
+        )
+    console.print(table)
+
+
+
 @command("show orders", "информация о заказах", CATEGORY_ORDERS, [ROLE_SALES_MANAGER])
 def show_order(_id: str) -> None:
     order = _get_order(int(_id))
